dfe17ae49cb7e271a27630f9fa9994679961e700
hugoBR2710 <hugosoares2710@outlook.pt>
2023-11-11 21:37:08 +0000

    teste

---
+++ Museu/Museu/Visitante.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Museu
+{
+    // Classe Visitante derivada de Pessoa
+    class Visitante : Pessoa
+    {
+        public string MotivoVisita { get; set; }
+
+        public Visitante(string nome, int idade, string motivoVisita) : base(nome, idade)
+        {
+            MotivoVisita = motivoVisita;
+        }
+
+        public override void Apresentar()
+        {
+            Console.WriteLine($"Olá, sou um visitante chamado {Nome}, tenho {Idade} anos e estou aqui para {MotivoVisita}.");
+        }
+    }
+}
