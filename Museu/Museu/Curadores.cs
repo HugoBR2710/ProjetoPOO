cff863b80584e9a51ae1195c8a2a87131a5e3477
hugoBR2710 <hugosoares2710@outlook.pt>
2023-11-11 19:49:27 +0000

    Criação e atualização de classes

---
+++ Museu/Museu/Curadores.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Museu
+{
+    class Curadores : Funcionario
+    {
+        public string AreaEspecializacao { get; set; }
+
+        public Curadores(string nome, int idade, string areaEspecializacao) : base (nome, idade, "Curador de Arte")
+        {
+            Nome = nome;
+            Idade = idade;
+            Cargo = "Curador de Arte";
+            AreaEspecializacao = areaEspecializacao;
+        }
+
+        public void Inventariar()
+        {
+            Console.WriteLine($"Curador {Nome} está a efetuar o registo de obras de arte.");
+        }
+    }
+}
--- Museu/Museu/Program.cs
+++ Museu/Museu/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +7,20 @@ namespace Museu
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            // Exemplo de uso
+            var funcionario = new Funcionario("João", 30, "gestor");
+            var visitante = new Visitante("Maria", 25, "Conhecer o museu");
+            var curador = new Curadores("Ana", 35, "Pinturas Renascentistas");
+            var seguranca = new Seguranca("Carlos", 28);
+
+            
+
+            funcionario.Apresentar();
+            visitante.Apresentar();
+            curador.Inventariar();
+            seguranca.ControlarAcesso();
         }
     }
 }
